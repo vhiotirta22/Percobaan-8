3665fa0e35a64915aeb8d16fbd599e8fb6b5814a
M S I <vhiotirta22@gmail.com>
2023-12-18 22:50:44 +0800

    Intial Commit

---
+++ app/src/main/java/com/example/firebasefirestore/ListActivity.java
@@ -0,0 +1,42 @@
+package com.example.firebasefirestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.percobaan6vhiotirta.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ListActivity extends AppCompatActivity {
+    private ArrayList<String> dataNama;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+        dataNama = new ArrayList<>();
+        ListView listView = findViewById(R.id.list_view);
+
+        mFirestore.collection("pengguna").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())){
+                    dataNama.add(document.getString("nama") + " - " + document.getString("stambuk"));
+                }
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, dataNama);
+                listView.setAdapter(adapter);
+            } else {
+                Log.w("Lihat", "Error getting documents.", task.getException());
+            }
+        });
+    }
+}
